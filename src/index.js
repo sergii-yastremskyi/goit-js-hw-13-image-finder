556c2c62e11e5cf5d78017c2f5b2cf429756983a
sergii_yastremskyi <theyastremsky@gmail.com>
2021-07-19 08:54:16 +0300

    post

--- src/index.js
+++ src/index.js
@@ -1,1 +1,99 @@
 import './sass/main.scss';
+
+const refs = {
+    input: document.querySelector("#search-form > input[type=text]"),
+    form: document.querySelector("#search-form"),
+    loadMO: document.querySelector("#pagination"),
+    gallery: document.querySelector('.gallery')
+}
+
+ const createCardElement = (iconName,data,container)=>{
+     const element = document.createElement('P');
+     element.className = 'stats-item';
+            const innerElement = document.createElement('I');
+            innerElement.textContent = iconName;
+            innerElement.className = 'material-icons';
+            element.innerHTML = data;
+            element.appendChild(innerElement);
+            container.appendChild(element);
+        }
+
+
+class GalleryApp {
+    constructor() {
+        this.pageNumber = 1;
+        this.searchValue = '';
+       
+    }
+  
+    setSearchValue(val) {
+        
+        this.searchValue = val;
+        this.pageNumber = 1;
+        if (val === '') {
+            refs.gallery.innerHTML = '';
+            refs.loadMO.classList.add('hidden');
+        }
+        else {
+            this.fetchItems();
+            refs.loadMO.classList.remove('hidden');
+        }
+             
+    }
+    fetchItems() {
+        fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchValue}&page=${this.pageNumber}&per_page=12&key=22553611-d17142b90db34a0c793ad1fbe`)
+            .then(res => res.json())
+            .then(data => {
+                
+                this.renderGallery(data.hits);
+            })
+            .catch(error => console.log(error));
+        
+    }
+    increasePagenumber() {
+        this.pageNumber++;
+        this.fetchItems();
+    }
+    renderCard(itemdata) {
+        const container = document.createElement('DIV');
+       
+        
+        container.className = 'photo-card';
+        const image = document.createElement('IMG');
+        image.src = itemdata.webformatURL;
+        container.appendChild(image);
+        const innercontainer = document.createElement('DIV');
+        innercontainer.className = 'stats';
+        createCardElement('thumb_up',itemdata.likes,innercontainer);
+        createCardElement('visibility',itemdata.views,innercontainer);
+        createCardElement('comments',itemdata.comments,innercontainer);
+        createCardElement('cloud_download', itemdata.downloads, innercontainer);
+        console.log(container);
+        container.appendChild(innercontainer);
+        refs.gallery.appendChild(container);
+    }
+    renderGallery(itemsarray) {
+        itemsarray.map(item => this.renderCard(item));
+       
+        setTimeout(() => {  refs.loadMO.scrollIntoView({
+          behavior: 'smooth',
+            block: 'end',
+        })
+        },500)
+    }
+
+}
+
+const myGallery = new GalleryApp();
+
+refs.form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    myGallery.setSearchValue(refs.input.value);
+});
+
+refs.loadMO.addEventListener('click', (e) => {
+    e.preventDefault();
+    myGallery.increasePagenumber();
+});
+
+
